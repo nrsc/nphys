1f00b5b96adfcbd371ce92860348b9fd547fbdb0
NRSC <sd.sawchuk@gmail.com>
2019-05-10 22:01:22 -0700

    Updated run file

---
+++ run.R
@@ -0,0 +1,40 @@
+tst = sA$Data[1]
+names(tst)
+
+sapply(as.data.frame(tst)[,-1], function(x, SA = 1000){
+df = NULL
+tst <- data.frame(x)
+tst1 <- data.matrix(tst)
+tst2 <- tst1[,ncol(tst1)]
+tst3 <- bl_adjust(tst2)
+#plot(1:length(tst3), tst3)
+#tst4 <- slpRead(tst3)
+#if(which.max(tst4) > 1500){SA = 1500}
+#if(which.max(tst4) < 1500){SA = 1000}
+tst5 = tst3[SA:(SA+7000)]
+
+df <- cbind(df, tst5)
+return(df)
+
+}) %>%
+    melt(., value.name = "mV") %>%
+    ggplot(., aes(Var1, mV, colour = Var2)) +
+    #stat_summary(fun.y = mean, geom = "line") +
+    geom_line() +
+    #scale_color_grey(start = 0.8, end = 0.2)+
+    ylim(-2, 0.2) +
+    theme_pubclean()+
+    theme(legend.position = "none")
+
+
+df_mean <- rowMeans(df)
+dfmelt_mean <- melt(df_mean, value.name = "mV")
+
+
+
+
+
+
+rownames(tst)
+
+

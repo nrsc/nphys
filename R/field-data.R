4fb42a42690d5a3e07df85d4429bbe17b0514d5a
Scott <scott@nrsccollective.com>
2020-04-24 19:09:30 -0700

    Update Build check

--- R/field-data.R
+++ R/field-data.R
@@ -1,23 +1,18 @@
 #' Field Data Set
-#'
 #' @docType data
 #'
-#' @usage data(field)
-#'
-#' @keywords datasets
-#'
-#' @format a nested list with 5 elements
-#'
-#' @description  My own data set outlining a normal field electrophysiology experiment to asses long-term depression in the hippocampus
-#'
-#' @source Christie Laboratory, Victoria B.C.
-#'
-#' @examples
-#' \dontrun{
-#' data(field)
-#' names(field$ABF)
+#' @format a \code{nested.list} with 5 elements:
+#' \describe{
+#'  \item{wd}{working directory}
+#'  \item{md}{Experimental metadata}
+#'  \item{files}{Imported files}
+#'  \item{ABF}{Imported data from files}
+#'  \item{traces}{example traces}
 #' }
 #'
-#'
+#' @description  My own data set outlining a normal field electrophysiology experiment to asses long-term depression in the hippocampus
+#' @source Christie Laboratory, Victoria B.C.
+#' @keywords datasets
+#' @usage data(field)
 #'
 "field"

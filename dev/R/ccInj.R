314c49b1a1f0e662c46024c83a4b684852dc5a38
Scott Sawchuk <sd.sawchuk@gmail.com>
2020-05-28 15:58:43 -0700

    dev update 0.0.11

---
+++ dev/R/ccInj.R
@@ -0,0 +1,22 @@
+#' Current clamp inject On
+#' This function identifies where the current is injected
+#'
+#' @param x a vector. Current signal for current clamp recordings
+#' @param testPulse Logical. Is there a test pulse in the trace
+#' @param tpPos Is the test pulse before or after
+#'
+#' @return list. index points along trace that represents where the current signal turns on and off
+#' @export ccInj
+#'
+#' @examples
+#' \dontrun{
+#'
+#' }
+ccInj <- function(x, I = Imon, V = Vmon, testPulse = TRUE, tpPos = "pre", AD = FALSE){
+
+
+
+
+
+
+}

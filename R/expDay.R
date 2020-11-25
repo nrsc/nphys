6e5343d8e59175a26e147103ff568ea04004e181
Scott Sawchuk <sd.sawchuk@gmail.com>
2020-11-25 12:09:10 -0800

    change folder structures

---
+++ R/expDay.R
@@ -0,0 +1,39 @@
+#' Experimental day
+#'
+#' @return
+#' @export expDay
+#'
+#' @examples
+expDay <- function(x){
+  if(missing(x)) select.list(tools::file_path_sans_ext(list.files("exps")))
+
+  Params <- readODS::read_ods("exp/Params.ods", col_types = cols())
+
+md <- MetaData()
+print(md)
+
+if(select.list(c("Yes", "No"), title = "Details Correct?") == "No"){
+    md <- MetaData()
+    print(md)
+}
+
+if(select.list(c("Yes", "No"), title = "Write to Table?") == "Yes"){
+
+        write.table(md, file = list.files(pattern = "MD.csv", recursive = TRUE), sep = ",", append = TRUE, quote = FALSE,
+                col.names = FALSE, row.names = FALSE)
+        print("projRTT_MD updated")
+    }
+
+MD <- read.csv(file = "data/projRTT_MD.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
+
+if(select.list(c("Yes", "No"), title = "DataPath") == "Yes"){
+    Path = file.path(Params$DataPath[1], as.character(md$Date))
+    ifelse(!dir.exists(Path),dir.create(Path), FALSE)
+    }
+
+# if(select.list(c("Yes", "No"), title = "New Slice") == "Yes"){
+#     newSlice()
+# }
+
+}
+
